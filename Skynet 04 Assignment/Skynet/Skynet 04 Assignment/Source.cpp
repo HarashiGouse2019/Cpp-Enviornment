d5dfb6186660c4b5d8c87dca1ff7360141b67e33
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-01-24 16:53:37 -0700

    What is this all about son?!?!?!

--- Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
+++ Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main() {
 
+	int t = 23;
+	int m = 87;
+
+
 	cout << "HK Software" << endl;
+
+	t > m ? cout << "Yes!!!" << endl : cout << "No!!!" << endl;
 
 	system("pause");
 
