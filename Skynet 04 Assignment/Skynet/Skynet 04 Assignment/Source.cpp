adc2af1aced29f257601b628eb9171efa18f136e
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-01-24 17:01:11 -0700

    Update Source.cpp

--- Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
+++ Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main() {
 
+	int searchGridHighNumber = 64;
+	int searchGridLowNumber = 1;
+
 	cout << "HK Software" << endl;
 	//This is the key algorithm we will use. It is called Binary Search.
 
@@ -13,8 +16,11 @@ int main() {
 		for (;;) {}
 	}
 	
-	// int targetLocation Prediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
+	//int targetLocation Prediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
 	system("pause");
+
+	//Search Grid High #64
+	//Search Grid Low #1
 
 	return 0;
 }
