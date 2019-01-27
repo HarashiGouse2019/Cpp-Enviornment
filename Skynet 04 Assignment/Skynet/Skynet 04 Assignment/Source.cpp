c589bdb9e5ebf2e480bb49a01e14851a50addf16
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-01-27 01:02:57 -0700

    User Input!!!

--- Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
+++ Skynet 04 Assignment/Skynet/Skynet 04 Assignment/Source.cpp
@@ -4,75 +4,174 @@
 
 using namespace std;
 
+//I used a #define preprocessor just so I can do the lines without always holding the "=" key
+//and they can always be a constant amount. Using preprocessors are so much better!!!
 #define BREAKLINE cout << "==================================================" << endl;
 
 int main() {
 
-	int searchGridHighNumber = 64, searchGridLowNumber = 1, targetLocationPredictionCounter = 1, targetLocationPrediction, enemyLocation = rand() % searchGridHighNumber - searchGridLowNumber;
+	//We'll add some flair into this good! We'll define a grid inputted by the user that is greater or equal to 8 squared
+	//So they can have a bigger sector to learn for the enemy, and I find that very interesting!!!
+	
+	//So from here, we'll create a new variable (and we'll simply call it defineSectorSize)
+
+	int defineSectorSize;
 	
 	bool isTargetFound = false;
 
 	srand(time(0));
 
 	cout << "HK Software" << endl;
-	//This is the key algorithm we will use. It is called Binary Search.
-	cout << "Generate Randome enemy location on 8x8 grid..." << endl;
-	cout << "The enemy is located at location #" << enemyLocation << " on 8x8 grid with 1-64 sectors." << endl;
-	cout << "Skynet HK-Aerial Initializing software....." << endl;
 
-	BREAKLINE;
-	
-	//Keeping this blank for now.
-	while (isTargetFound == false) {
-		
-		
+	//We'll initialize some of the other variables here!
+	//But first, we'll ask the user to input a number to define the area of the sector.
 
-		//Software selection target location predicition in flyover hunt for enemy.
-		cout << "Skynet HK-Aerial Radar sending out ping #" << targetLocationPredictionCounter << endl;
+	getSectorSize:
+	cout << "Define the size of the sector (Sector size can not be smaller than four)" << endl;
+	cin >> defineSectorSize;
 
-		// Radar Ping of enemy location
-		
-		targetLocationPrediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
+	//We'll then check if the input was a valid one using an if statement
+	if (defineSectorSize < 4) {
+		cout << "The Sector size that you have entered is invalid..." << endl;
+		goto getSectorSize;
 
-		// In this simulation the ping tells us
-		// if the target location is higher or lower or right on the enem location.
-		// I starget location prediction higher than enemy location
-
-		if (targetLocationPrediction > enemyLocation) {
-			//The target Location Prediction was higher than the enemyLocation
-			//no need to search anymore at or above target location prediction
-			//reset the searchGridHighNumber of the search Grid.
-			//We subtract 1 because we don't need to search the number guessed.
-			cout << "The target location prediction of " << targetLocationPrediction << " was higher than the actual enemy location of " << enemyLocation << endl;
-			searchGridHighNumber = targetLocationPrediction - 1;
-			cout << "searchGridHighNumber: " << searchGridHighNumber << endl;
-			targetLocationPredictionCounter++; //targetLocationPredictionCounter = targetLocationPredictionCounter + 1;
-
-			BREAKLINE;
-
-		}
-		else if (targetLocationPrediction < enemyLocation) {
-			//The target Location  Prediction was Lower than the enemyLocation
-			//no need to search anymore at or below the target location prediction
-			//We add 1 because we don't need to search the number guessed.
-			cout << "The target location prediction of " << targetLocationPrediction << " was lower than the actual enemy location of " << enemyLocation << endl;
-			searchGridLowNumber = targetLocationPrediction + 1;
-			cout << "searchGridLowNumber: " << searchGridLowNumber << endl;
-			targetLocationPredictionCounter++; //targetLocationPredictionCounter = targetLocationPredictionCounter + 1;
-
-			BREAKLINE;
-
-		}
-		else {
-			cout << "Enemy was hiding at Sector #" << enemyLocation << endl;
-			cout << "Target was found at Sector #" << enemyLocation << endl;
-			cout << "Skynet took " << targetLocationPredictionCounter << " predictions to find the enemy location on a grid size of 8x8 (64)" << endl;
-			isTargetFound = true;
-		}
+		/*They say that using a goto statement to jump to one code to another is considered to be bad code,
+		but for this example I feel like I should use it since it's the quickest, despite the reputation it has.*/
 	}
+	else {
 
-	system("pause");
+		//We'll save the area of the sector in a seperate variable
+		int sectorArea = defineSectorSize * defineSectorSize;
+
+		//searchGridHighNumber will be the Sector Size squared. If the user so happened to put in 8, searchGridHighNumber would be 64.
+		int searchGridHighNumber = defineSectorSize * defineSectorSize,
+			searchGridLowNumber = 1,
+			//searchGridLowNumber while always equal to 1 no matter what!!!
+
+			targetLocationPredictionCounter = 1,
+			targetLocationPrediction,
+			enemyLocation = rand() % searchGridHighNumber - searchGridLowNumber;
+
+		//This is the key algorithm we will use. It is called Binary Search.
+		cout << "Generate Randome enemy location on "
+			<< defineSectorSize
+			<< "x"
+			<< defineSectorSize
+			<< " grid..."
+			<< endl;
+
+		cout << "The enemy is located at location #"
+			<< enemyLocation
+			<< " on "
+			<< defineSectorSize
+			<< "x"
+			<< defineSectorSize
+			<< " grid with 1-"
+			<< sectorArea
+			<< " sectors."
+			<< endl;
+
+		cout << "Skynet HK-Aerial Initializing software....."
+			<< endl;
+
+		BREAKLINE;
+
+		//Keeping this blank for now.
+		while (isTargetFound == false) {
+
+
+
+			//Software selection target location predicition in flyover hunt for enemy.
+			cout << "Skynet HK-Aerial Radar sending out ping #"
+				<< targetLocationPredictionCounter
+				<< endl;
+
+			// Radar Ping of enemy location
+
+			targetLocationPrediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
+
+			// In this simulation the ping tells us
+			// if the target location is higher or lower or right on the enem location.
+			// I starget location prediction higher than enemy location
+
+			if (targetLocationPrediction > enemyLocation) {
+				//The target Location Prediction was higher than the enemyLocation
+				//no need to search anymore at or above target location prediction
+				//reset the searchGridHighNumber of the search Grid.
+				//We subtract 1 because we don't need to search the number guessed.
+				cout << "The target location prediction of "
+					<< targetLocationPrediction
+					<< " was higher than the actual enemy location of "
+					<< enemyLocation
+					<< endl;
+
+				searchGridHighNumber = targetLocationPrediction - 1;
+
+				cout << "searchGridHighNumber: "
+					<< searchGridHighNumber
+					<< endl;
+
+				targetLocationPredictionCounter++; //targetLocationPredictionCounter = targetLocationPredictionCounter + 1;
+
+				BREAKLINE;
+
+			}
+			else if (targetLocationPrediction < enemyLocation) {
+				//The target Location  Prediction was Lower than the enemyLocation
+				//no need to search anymore at or below the target location prediction
+				//We add 1 because we don't need to search the number guessed.
+				cout << "The target location prediction of "
+					<< targetLocationPrediction
+					<< " was lower than the actual enemy location of "
+					<< enemyLocation
+					<< endl;
+
+				searchGridLowNumber = targetLocationPrediction + 1;
+
+				cout << "searchGridLowNumber: "
+					<< searchGridLowNumber
+					<< endl;
+
+				targetLocationPredictionCounter++; //targetLocationPredictionCounter = targetLocationPredictionCounter + 1;
+
+				BREAKLINE;
+
+			}
+			else {
+				cout << "Enemy was hiding at Sector #"
+					<< enemyLocation
+					<< endl;
+
+				cout << "Target was found at Sector #"
+					<< enemyLocation
+					<< endl;
+
+				cout << "Skynet took "
+					<< targetLocationPredictionCounter
+					<< " predictions to find the enemy location on a grid size of " 
+					<< defineSectorSize 
+					<< "x" 
+					<< defineSectorSize 
+					<< "(" 
+					<< sectorArea 
+					<< ")"
+					<< endl;
+
+				isTargetFound = true;
+
+			}
+		}
+
+		system("pause");
+
+	}
 
 	return 0;
 
 	}
+
+	//I broke up the cout functions the way they are because I find them going outside of the editor field
+	//so doing this, no one has to scroll horizontally just to see the string.
+	//It's been compressed every time << is used.
+	//Honestly the code shouldn't be 171 lines long, but because I wanted everything
+	//to show in one area instead of out of bounds, that's what happened.
