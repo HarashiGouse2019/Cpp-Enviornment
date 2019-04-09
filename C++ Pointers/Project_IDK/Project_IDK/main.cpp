66d96eff90e4d6be300219e45945aef0971552e5
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-04-08 20:44:29 -0600

    Update main.cpp

--- C++ Pointers/Project_IDK/Project_IDK/main.cpp
+++ C++ Pointers/Project_IDK/Project_IDK/main.cpp
@@ -1,3 +1,19 @@
+/****************************************************************************************************************************************************************
+*														WELCOME TO THE GAME OF PURSUIT!!!!																		*
+*																																								*
+*										This is the first program where I've actually used pointers in my code.													*
+*										I honestly didn't know what I should write a program of at first, until													*
+*										this idea happened.																										*
+*																																								*	
+*										Putting together my current knowledge of C++, I created a simple console												*
+*										where you are given a coordinate, and you go to it based on sound.														*
+*																																								*
+*										It actually get's harder when you get closer, for you can barely hear													*
+*										anything. With that, you have to take smaller steps.																	*
+*																																								*
+*										You can play this game for as long as you like until you are satisfied.													*
+****************************************************************************************************************************************************************/
+
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -11,21 +27,21 @@ using namespace std;
 
 //Creates a line
 #define BREAK cout << "-----------------------------------------------------------------------------------------------------------" << endl;
-#define KEY_UP 72 //ASCII CODE FOR THE UP KEY
-#define KEY_DOWN 80 //ASCII CODE FOR THE DOWN KEY
-#define KEY_LEFT 75 //ASCII CODE FOR THE LEFT KEY
-#define KEY_RIGHT 77 //ASCII CODE FOR THE RIGHT KEY
-#define SPACE 32 //ASCII CODE FOR THE SPACE BAR
-#define RETURN 13 //ASCII CODE FOR THE ENTER/RETURN KEY
-#define ALT_UNIT Beep(1000, 50); Beep(1000, 50); //BEEPS TWO TIMES WITH A FREQUENCY OF 1000 AND A DURATION OF 50 MILLISECONDS
-#define SHOW_POS Beep(500, 50); Beep(500, 50); //BEEPS TWOS TIME WITH A FREQUENCY OF 500 AND A DURATION OF 50 MILLISECONDS
-#define MATCH Beep(2000, 200); //BEEPS ONE TIME WITH A FREQUENCY OF 2000 AND A DURATION OF 200 MILLISECONDS
+#define KEY_UP 0x48 //ASCII CODE (72) FOR THE UP KEY
+#define KEY_DOWN 0x50 //ASCII CODE (80) FOR THE DOWN KEY
+#define KEY_LEFT 0x4B //ASCII CODE (75) FOR THE LEFT KEY
+#define KEY_RIGHT 0x4D //ASCII CODE (77) FOR THE RIGHT KEY
+#define SPACE 0x20 //ASCII CODE (32) FOR THE SPACE BAR
+#define RETURN 0x0D //ASCII CODE (13) FOR THE ENTER/RETURN KEY
+#define ALT_UNIT Beep(0x03E8, 0x32); Beep(0x03E8, 0x32); //BEEPS TWO TIMES WITH A FREQUENCY OF 1000 AND A DURATION OF 50 MILLISECONDS
+#define SHOW_POS Beep(0x1F4, 0x32); Beep(0x1F4, 0x32); //BEEPS TWOS TIME WITH A FREQUENCY OF 500 AND A DURATION OF 50 MILLISECONDS
+#define MATCH Beep(0x7D0, 0xC8); //BEEPS ONE TIME WITH A FREQUENCY OF 2000 AND A DURATION OF 200 MILLISECONDS
 
 
 //Create a class called Sector
 class Sector {
 public:
-	unsigned int sectorSize = 1000; //The Default Sector Size
+	unsigned int sectorSize = 10; //The Default Sector Size
 	const unsigned int height = sectorSize; //The height of the sector
 	const unsigned int width = sectorSize; //The width of the sector
 };
@@ -85,7 +101,7 @@ int main(int argc, char** argv[]) {
 			system("CLS"); //Clear the console
 			cout << "--- CURRENTLY IN PURSUIT---" << endl; //Print out CURRENTLY IN PURSUIT
 			*ptry += unitSize; //MoveUp
-			Beep((max(*ptry, enemy.coord_y) - min(*ptry, enemy.coord_y)) * 10, 100); //Send a sound to let us know that we are getting closer
+			Beep(((max(*ptry, enemy.coord_y) - min(*ptry, enemy.coord_y)) % 0x7FFF), 0x64); //Send a sound to let us know that we are getting closer
 			break;
 
 		case KEY_DOWN:
@@ -93,7 +109,7 @@ int main(int argc, char** argv[]) {
 			system("CLS"); //Clear the console
 			cout << "--- CURRENTLY IN PURSUIT---" << endl; //Print out CURRENTLY IN PURSUIT
 			*ptry -= unitSize; //MoveDown
-			Beep((max(*ptry, enemy.coord_y) - min(*ptry, enemy.coord_y)) * 10, 100); //Send a sound to let us know that we are getting closer
+			Beep(((max(*ptry, enemy.coord_y) - min(*ptry, enemy.coord_y)) % 0x7FFF), 0x64); //Send a sound to let us know that we are getting closer
 			break;
 
 		case KEY_LEFT:
@@ -101,7 +117,7 @@ int main(int argc, char** argv[]) {
 			system("CLS"); //Clear the console
 			cout << "--- CURRENTLY IN PURSUIT---" << endl; //Print out CURRENTLY IN PURSUIT
 			*ptrx -= unitSize; //MoveLeft
-			Beep((max(*ptrx, enemy.coord_x) - min(*ptrx, enemy.coord_x)) * 10, 100); //Send a sound to let us know that we are getting closer
+			Beep(((max(*ptrx, enemy.coord_x) - min(*ptrx, enemy.coord_x)) % 0x7FFF), 0x64); //Send a sound to let us know that we are getting closer
 			break;
 
 		case KEY_RIGHT:
@@ -109,7 +125,7 @@ int main(int argc, char** argv[]) {
 			system("CLS"); //Clear the console
 			cout << "--- CURRENTLY IN PURSUIT---" << endl; //Print out CURRENTLY IN PURSUIT
 			*ptrx += unitSize; //MoveRight
-			Beep((max(*ptrx, enemy.coord_x) - min(*ptrx, enemy.coord_x)) * 10, 100); //Send a sound to let us know that we are getting closer
+			Beep(((max(*ptrx, enemy.coord_x) - min(*ptrx, enemy.coord_x)) % 0x7FFF), 0x64); //Send a sound to let us know that we are getting closer
 			break;
 
 		case SPACE:
@@ -155,8 +171,8 @@ int main(int argc, char** argv[]) {
 
 		//If both x and y coordinates are matching
 		if (*ptrx == enemy.coord_x && *ptry == enemy.coord_y) {
-			sector.sectorSize *= 10;
 			MATCH; MATCH_X = false; MATCH_Y = false; //Make a noise and set both MATCH_X, and MATCH_Y to FALSE
+			sector.sectorSize *= 10;
 			enemy.coord_x = rand() % sector.sectorSize; //Regenerate a new location on the x coordinate
 			enemy.coord_y = rand() % sector.sectorSize; //Regenerate a new location on the y coordinate
 			DisplayText("You've encountered the enemy, but he got away!"); //A message to use
